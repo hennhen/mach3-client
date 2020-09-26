12da38124b1200537c6e37570873328ecd1d522f
Henry Wu <henrywuu1122@gmail.com>
2020-09-26 20:08:53 +0800

    yeet

--- server/io.ts
+++ server/io.ts
@@ -1,25 +1,28 @@
-import sio, { Server, Socket } from 'socket.io';
-import { getSocket, set } from './socket';
+import sio, { Server, Socket } from "socket.io";
+import { getSocket, set } from "./socket";
 
 let io: Server | undefined = undefined;
 
 export const getIO = () => {
-  return io;
+   return io;
 };
 
 export const initialize = (server: any) => {
-  io = sio(server);
-  io.on('connection', (socket: Socket) => {
-    const localSocket = getSocket();
-    if (localSocket) {
-      console.log('client socket connected');
-      socket.on('rtc', (signal: string) => {
-        localSocket.emit('rtc', { signal, id: socket.id });
-      });
-    } else {
-      console.log('local socket connected');
-      socket = set(socket);
-    }
-  });
-  return io;
+   io = sio(server);
+   io.on("connection", (socket: Socket) => {
+      const localSocket = getSocket();
+      if (localSocket) {
+         console.log("client socket connected");
+         socket.on("rtc", (signal: string) => {
+            localSocket.emit("rtc", { signal, id: socket.id });
+         });
+         socket.on("disconnect", () => {
+            console.log("client socket disconnected");
+         });
+      } else {
+         console.log("local socket connected");
+         socket = set(socket);
+      }
+   });
+   return io;
 };
--- src/containers/Dashboard.tsx
+++ src/containers/Dashboard.tsx
@@ -1,112 +1,126 @@
-import React, { useRef, useEffect } from 'react';
-import { Container, Grid } from '@material-ui/core';
-import io from 'socket.io-client';
-import Peer, { Instance } from 'simple-peer';
-import { Video } from '../components';
+import React, { useRef, useEffect } from "react";
+import { Container, Grid } from "@material-ui/core";
+import io from "socket.io-client";
+import Peer, { Instance } from "simple-peer";
+import { Video } from "../components";
 
 const Dashboard = () => {
-  const videoOne = useRef<HTMLVideoElement>(null);
-  const videoTwo = useRef<HTMLVideoElement>(null);
-  const socket = useRef<any>(null);
-  const peers = useRef<{ [id: string]: Instance }>({});
+   const videoOne = useRef<HTMLVideoElement>(null);
+   const videoTwo = useRef<HTMLVideoElement>(null);
+   const socket = useRef<any>(null);
+   const peers = useRef<{ [id: string]: Instance }>({});
 
-  const buildPeer = (id: string) => {
-    const peer = new Peer({
-      initiator: false
-    });
+   const buildPeer = (id: string) => {
+      const streams: MediaStream[] = [];
 
-    if (videoOne.current)
-      peer.addStream(videoOne.current.srcObject as MediaStream);
+      if (videoOne.current)
+         streams.push(videoOne.current.srcObject as MediaStream);
 
-    if (videoTwo.current)
-      peer.addStream(videoTwo.current.srcObject as MediaStream);
+      if (videoTwo.current)
+         streams.push(videoTwo.current.srcObject as MediaStream);
 
-    peer.on('signal', (signal) => {
-      socket.current.emit('rtc', { signal, id });
-    });
+      const stream = videoOne.current
+         ? (videoOne.current.srcObject as MediaStream)
+         : undefined;
 
-    peer.on('error', (error) => {
-      console.error(error);
-    });
-
-    peer.on('close', () => {
-      console.log('peer closed');
-      const newPeers = { ...peers.current };
-      delete newPeers[id];
-      peers.current = newPeers;
-    });
-
-    peer.on('connect', () => {
-      console.log('peer connected');
-    });
-
-    return peer;
-  };
-
-  useEffect(() => {
-    const connectVideo = async () => {
-      if (!videoOne.current || !videoTwo.current) return;
-
-      await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
-
-      const deviceIDs: string[] = [];
-
-      const devices = await navigator.mediaDevices.enumerateDevices();
-
-      devices.forEach((device) =>
-        device.kind === 'videoinput' ? deviceIDs.push(device.deviceId) : null
-      );
-
-      for (let idx = 0; idx < deviceIDs.length; idx++) {
-        const id = deviceIDs[idx];
-
-        const stream = await navigator.mediaDevices.getUserMedia({
-          video: {
-            deviceId: { exact: id }
-          },
-          audio: idx === 0
-        });
-        if (idx === 0) videoOne.current.srcObject = stream;
-        else if (idx === 1) videoTwo.current.srcObject = stream;
-      }
-    };
-
-    connectVideo();
-  }, [videoOne, videoTwo]);
-
-  useEffect(() => {
-    const socketConnect = async () => {
-      socket.current = await io('http://localhost:5000');
-      socket.current.on('connect', () => {
-        console.log('CONNECTED');
+      const peer = new Peer({
+         initiator: false,
+         stream: stream,
       });
-      socket.current.on('rtc', (dataString: string) => {
-        const { id, signal } = JSON.parse(dataString);
-        let peer: Instance;
-        if (!peers.current[id]) {
-          peer = buildPeer(id);
-          const newPeers = { ...peers.current, id: peer };
-          peers.current = newPeers;
-        } else peer = peers.current[id];
-        peer.signal(signal);
+
+      peer.on("signal", (signal) => {
+         socket.current.emit("rtc", { signal, id });
       });
-    };
 
-    socketConnect();
-  }, []);
+      peer.on("error", (error: any) => {
+         console.error(error.code);
+      });
 
-  return (
-    <Container>
-      <Grid container spacing={3}>
-        <Grid item xs={6}>
-          <Video ref={videoOne} />
-        </Grid>
-        <Grid item xs={6}>
-          <Video ref={videoTwo} />
-        </Grid>
-      </Grid>
-    </Container>
-  );
+      peer.on("close", () => {
+         console.log("peer closed");
+         const newPeers = { ...peers.current };
+         delete newPeers[id];
+         peers.current = newPeers;
+      });
+
+      peer.on("connect", () => {
+         console.log("peer connected");
+      });
+
+      return peer;
+   };
+
+   useEffect(() => {
+      const connectVideo = async () => {
+         if (!videoOne.current || !videoTwo.current) return;
+
+         await navigator.mediaDevices.getUserMedia({
+            audio: true,
+            video: true,
+         });
+
+         const deviceIDs: string[] = [];
+
+         const devices = await navigator.mediaDevices.enumerateDevices();
+
+         devices.forEach((device) =>
+            device.kind === "videoinput"
+               ? deviceIDs.push(device.deviceId)
+               : null
+         );
+
+         for (let idx = 0; idx < deviceIDs.length; idx++) {
+            const id = deviceIDs[idx];
+
+            const stream = await navigator.mediaDevices.getUserMedia({
+               video: {
+                  deviceId: { exact: id },
+               },
+               audio: idx === 0,
+            });
+            if (idx === 0) videoOne.current.srcObject = stream;
+            else if (idx === 1) videoTwo.current.srcObject = stream;
+         }
+      };
+
+      connectVideo();
+   }, [videoOne, videoTwo]);
+
+   useEffect(() => {
+      const socketConnect = async () => {
+         socket.current = await io("http://localhost:5000");
+         socket.current.on("connect", () => {
+            console.log("CONNECTED");
+         });
+         socket.current.on("rtc", (data: { signal: string; id: string }) => {
+            const { id, signal } = data;
+            let peer: Instance;
+            if (!peers.current[id]) {
+               console.log(id);
+               peer = buildPeer(id);
+               const newPeers = { ...peers.current };
+               newPeers[id] = peer;
+               peers.current = newPeers;
+            } else peer = peers.current[id];
+            peer.signal(signal);
+         });
+      };
+
+      socketConnect();
+   }, []);
+
+   return (
+      <Container>
+         <Grid container spacing={3}>
+            <Grid item xs={6}>
+               <Video ref={videoOne} />
+            </Grid>
+            <Grid item xs={6}>
+               <Video ref={videoTwo} />
+            </Grid>
+         </Grid>
+      </Container>
+   );
 };
 
 export { Dashboard };
