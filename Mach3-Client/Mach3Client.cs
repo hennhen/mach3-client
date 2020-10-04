a9e24146ee62b6bc1568fb85e2b5e8f5fbae44e6
Henry Wu <henrywuu1122@gmail.com>
2020-10-04 17:35:34 +0800

    added inputs to control mach3

--- Mach3-Client/Mach3Client.cs
+++ Mach3-Client/Mach3Client.cs
@@ -12,11 +12,13 @@ using Mach4;
 using Mach3_Client;
 using System.Threading;
 
-namespace Mach3_Interface_Server {
+namespace Mach3_Interface_Server
+{
 
     /// <summary>Class <c>Mach3Client</c> has the handle of Mach3Mill and uses Mach3 
     /// macro VBA functions for getting and setting data</summary>
-    class Mach3Client {
+    class Mach3Client
+    {
 
         // The JSON Object for storing outgoing data
         public Mach3OutgoingJSON outgoingJson = new Mach3OutgoingJSON();
@@ -41,27 +43,36 @@ namespace Mach3_Interface_Server {
         private SynchronizationContext _synchronizationContext;
 
         // Constructor
-        public Mach3Client() {
+        public Mach3Client()
+        {
             this._synchronizationContext = SynchronizationContext.Current;
-            try {
+            try
+            {
                 Mach3App = (Mach4.IMach4)Marshal.GetActiveObject("Mach4.Document");
                 Mach3 = (Mach4.IMyScriptObject)Mach3App.GetScriptDispatch();
-            } catch(COMException e) {
+            }
+            catch (COMException e)
+            {
                 throw (new Exception("Mach3NotFoundException"));
             }
         }
 
-        public async void executeCommand(string command) {
+        public async void executeCommand(string command)
+        {
 
-            switch (command) {
+            switch (command)
+            {
                 case "start":
                     Mach3.DoOEMButton(1000);
                     break;
                 case "pause":
                     Mach3.DoOEMButton(1001);
                     break;
-                case "zero-all":
-                    Mach3.DoOEMButton(1007);
+                case "stop":
+                    Mach3.DoOEMButton(1003);
+                    break;
+                case "estop":
+                    Mach3.DoOEMButton(1021);
                     break;
                 default:
                     Debug.Print("Received unknown command: " + command);
@@ -70,7 +81,8 @@ namespace Mach3_Interface_Server {
             // TODO: This blocks??? when we do open GCode Window
         }
 
-        private void getCoors() {
+        private void getCoors()
+        {
             outgoingJson.coors.x = Mach3.GetOEMDRO(DRO_X);
             outgoingJson.coors.y = Mach3.GetOEMDRO(DRO_Y);
             outgoingJson.coors.z = Mach3.GetOEMDRO(DRO_Z);
@@ -81,7 +93,8 @@ namespace Mach3_Interface_Server {
         /// <summary>
         /// Update the internal <code>outgoingJson</code> with each getter functions.
         /// </summary>
-        public void updateData() {
+        public void updateData()
+        {
             // TODO: Catch InvalidCastexception for when Mach3 closes suddenly
             getCoors();
 
@@ -100,7 +113,8 @@ namespace Mach3_Interface_Server {
         /// 
         /// <summary>Returns stringified <code>outgoingJson</code></summary>
         /// 
-        public string getJsonData() {
+        public string getJsonData()
+        {
             return JsonConvert.SerializeObject(outgoingJson);
         }
     }
